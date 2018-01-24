e8adb92a6d2b857b6e524ec110ba718a689a310f
startcaft <51418996@qq.com>
2018-01-24 21:34:02 +0800

    构思一个分解复杂业务的分支体系

---
+++ juc-framework-branchs/src/test/java/startcaft/juc/www/JucFrameworkBranchsApplicationTests.java
@@ -0,0 +1,16 @@
+package startcaft.juc.www;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JucFrameworkBranchsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
