4e1db75df8204f47687f55cf93fd1d63791a6844
startcaft <51418996@qq.com>
2018-02-17 17:14:56 +0800

    多线程的数据共享性的安全问题模拟

---
+++ juc-parent/juc-share-data/src/main/java/startcaft/juc/www/App.java
@@ -0,0 +1,61 @@
+package startcaft.juc.www;
+
+/**
+ * @Author: pikai
+ * @Description:
+ * @Date: Created in 2018/2/17 16:54
+ */
+public class App {
+
+    public static void main( String[] args) {
+
+        test1();// 每次执行的结果都不一样，也有可能有正确的结果。
+    }
+
+    public static void test1(){
+        final ShareData data = new ShareData();
+        // 开启十个线程，每个线程循环100次
+        for (int i = 0; i < 10 ; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        // 暂停1秒，增加并发问题出现的几率
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    for (int j = 0; j < 100 ; j++) {
+                        data.addCount();
+                    }
+
+                    System.out.print(data.getCount() + " ");
+                }
+            }).start();
+        }
+
+        try {
+            // 主线程暂停三秒，以保证上面分线程执行完毕。
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("count=" + data.getCount());
+    }
+}
+
+/**
+ * 共享数据类
+ */
+class ShareData{
+    private int count = 0;
+
+    public void addCount(){
+        this.count++;
+    }
+
+    public int getCount(){
+        return this.count;
+    }
+}
