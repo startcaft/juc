e2f082832e4b5aceb270edf634062b92d972a2f7
startcaft <51418996@qq.com>
2018-02-17 21:45:00 +0800

    线程对资源的访问具有唯一性和排它性，这叫互斥性

--- juc-parent/juc-share-data/src/main/java/startcaft/juc/www/App.java
+++ juc-parent/juc-share-data/src/main/java/startcaft/juc/www/App.java
@@ -10,6 +10,7 @@ public class App {
     public static void main( String[] args) {
 
         test1();// 每次执行的结果都不一样，也有可能有正确的结果。
+        test2();// 论执行多少次，返回的最终结果都是1000
     }
 
     public static void test1(){
@@ -43,6 +44,38 @@ public class App {
         }
         System.out.println("count=" + data.getCount());
     }
+
+    public static void test2(){
+        final ShareData data = new ShareData();
+        // 开启十个线程，每个线程循环100次
+        for (int i = 0; i < 10 ; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        // 暂停1秒，增加并发问题出现的几率
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    for (int j = 0; j < 100 ; j++) {
+                        data.syncAddCount();
+                    }
+
+                    System.out.print(data.getCount() + " ");
+                }
+            }).start();
+        }
+
+        try {
+            // 主线程暂停三秒，以保证上面分线程执行完毕。
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("count=" + data.getCount());
+    }
 }
 
 /**
@@ -57,5 +90,9 @@ class ShareData{
 
     public int getCount(){
         return this.count;
+    }
+
+    public synchronized  void syncAddCount(){
+        this.count++;
     }
 }
