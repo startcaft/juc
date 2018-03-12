4607e1d37c60f45745a74ddf74438cdac8fe40ba
startcaft <51418996@qq.com>
2018-03-12 10:23:09 +0800

    添加一个兼容版本的本地存储对象myStorage

---
+++ pro-parent/core-web-view/src/main/resources/static/script/myStorage.js
@@ -0,0 +1,29 @@
+//本地存储，localStorage类没有存储空间的限制，而cookieStorage有存储大小限制
+//在不支持localStorage的情况下会自动切换为cookieStorage
+window.myStorage = (new (function(){
+
+    var storage;    //声明一个变量，用于确定使用哪个本地存储函数
+
+    if(window.localStorage){
+        storage = localStorage;     //当localStorage存在，使用H5方式
+    }
+    else{
+        storage = cookieStorage;    //当localStorage不存在，使用兼容方式
+    }
+
+    this.setItem = function(key, value){
+        storage.setItem(key, value);
+    };
+
+    this.getItem = function(name){
+        return storage.getItem(name);
+    };
+
+    this.removeItem = function(key){
+        storage.removeItem(key);
+    };
+
+    this.clear = function(){
+        storage.clear();
+    };
+})());
